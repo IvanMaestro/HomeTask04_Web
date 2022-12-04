a993d9173a5f125b5cdbc1c44c48aff05f79d7ae
Ivan <109222249+IvanMaestro@users.noreply.github.com>
2022-12-04 11:13:23 +0000

    update task 2

--- Task02/my.sql
+++ Task02/my.sql
@@ -1,6 +1,6 @@
 
 -- create
-CREATE TABLE CLASSMATES (
+CREATE TABLE classmates (
   id INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
   age INTEGER NOT NULL,
@@ -8,16 +8,16 @@ CREATE TABLE CLASSMATES (
 );
 
 -- insert
-INSERT INTO CLASSMATES VALUES (0001, 'Василий', '18', 'Москва');
-INSERT INTO CLASSMATES VALUES (0002, 'Игнат', '34', 'Анапа');
-INSERT INTO CLASSMATES VALUES (0003, 'Нина', '17', 'Краснодар');
-INSERT INTO CLASSMATES VALUES (0004, 'Вика', '31', 'Москва');
-INSERT INTO CLASSMATES VALUES (0005, 'Семён', '19', 'Москва');
-INSERT INTO CLASSMATES VALUES (0006, 'Саша', '30', 'Москва');
-INSERT INTO CLASSMATES VALUES (0007, 'Белла', '23', 'Одинцово');
-INSERT INTO CLASSMATES VALUES (0008, 'Вова', '25', 'Москва');
-INSERT INTO CLASSMATES VALUES (0009, 'Толик', '20', 'Подольск');
-INSERT INTO CLASSMATES VALUES (0010, 'Елена', '37', 'Москва');
+INSERT INTO classmates VALUES (0001, 'Василий', '18', 'Москва');
+INSERT INTO classmates VALUES (0002, 'Игнат', '34', 'Анапа');
+INSERT INTO classmates VALUES (0003, 'Нина', '17', 'Краснодар');
+INSERT INTO classmates VALUES (0004, 'Вика', '31', 'Москва');
+INSERT INTO classmates VALUES (0005, 'Семён', '19', 'Москва');
+INSERT INTO classmates VALUES (0006, 'Саша', '30', 'Москва');
+INSERT INTO classmates VALUES (0007, 'Белла', '23', 'Одинцово');
+INSERT INTO classmates VALUES (0008, 'Вова', '25', 'Москва');
+INSERT INTO classmates VALUES (0009, 'Толик', '20', 'Подольск');
+INSERT INTO classmates VALUES (0010, 'Елена', '37', 'Москва');
 
 -- fetch 
-SELECT name FROM CLASSMATES WHERE address = 'Москва' and age >= 18 and age < 30;
+SELECT name FROM classmates WHERE address = 'Москва' AND age >= 18 and age < 30;
